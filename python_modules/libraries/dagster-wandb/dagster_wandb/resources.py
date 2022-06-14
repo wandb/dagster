17611c177bc6817998dfead49f42910c23a4b779
david.lachasse@gmail.com <david.lachasse@gmail.com>
2022-06-13 19:17:25 -0700

    Add explicit example of resource usage

--- python_modules/libraries/dagster-wandb/dagster_wandb/resources.py
+++ python_modules/libraries/dagster-wandb/dagster_wandb/resources.py
@@ -16,7 +16,35 @@ WANDB_CLOUD_HOST: str = 'https://api.wandb.ai'
     },
     description="Resource for interacting with the wandb SDK",
 )
-def wandb_resource(context: OpExecutionContext):
+def wandb_resource(context: OpExecutionContext) -> wandb:
+    """Resource for interacting with the wandb sdk.
+
+    Docs for the Python library can be found at: https://docs.wandb.ai/ref/python
+
+    Args:
+        context (OpExecutionContext): Execution context pass to all resources by default
+
+    Returns:
+        wandb: Authenticated wandb client
+
+    Example:
+        .. highlight:: python
+        .. code-block:: python
+
+            @op(required_resource_keys={'wandb'})
+            def do_some_nlp(context):
+                wandb = context.resources.wandb
+                headlines = ['Square(SQ) Surpasses Q4...', ...]
+                text_table = wandb.Table(columns=["Headline", "Positive", "Negative", "Neutral"])
+                for headline in headlines:
+                    pos_score, neg_score, neutral_score = model(headline)
+                text_table.add_data(headline, pos_score, neg_score, neutral_score)
+                wandb.log({"validation_samples" : text_table})
+
+            @job(resource_defs={'wandb': wandb_resource})
+            def wandb_job():
+                do_some_nlp()
+    """
     wandb.login(
         key=context.resource_config["api_key"],
         host=context.resource_config["host"],
